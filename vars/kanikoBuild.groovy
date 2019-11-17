224ec121da78abe0dda0c280ee425c159a7c4349
Louie Corbo <lcorbo@cloudbees.com>
2019-11-17 11:27:24 -0600

    Moving yaml to resource file.

--- vars/kanikoBuild.groovy
+++ vars/kanikoBuild.groovy
@@ -1,29 +1,7 @@
 #!/usr/bin/env groovy
 
 def call() {
-  podTemplate(yaml: """
-  kind: Pod
-  spec:
-    containers:
-    - name: kaniko
-      image: gcr.io/kaniko-project/executor:debug
-      imagePullPolicy: Always
-      command:
-      - /busybox/cat
-      tty: true
-      volumeMounts:
-        - name: jenkins-docker-cfg
-          mountPath: /kaniko/.docker
-    volumes:
-    - name: jenkins-docker-cfg
-      projected:
-        sources:
-        - secret:
-            name: regcred
-            items:
-              - key: .dockerconfigjson
-                path: config.json
-  """
+  podTemplate(yaml: libraryResource 'pods/kanikoBuild.yaml'
     ) {
     node(POD_LABEL) {
       checkout scm
