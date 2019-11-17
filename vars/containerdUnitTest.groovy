db92c991fd619223394aa4b0ab0a99d46a7e8759
Louie Corbo <lcorbo@cloudbees.com>
2019-11-17 17:04:49 -0600

    Testing unit testing phase

--- vars/containerdUnitTest.groovy
+++ vars/containerdUnitTest.groovy
@@ -5,7 +5,7 @@ def call() {
     node(POD_LABEL) {
       checkout scm
       container('dgoss') {
-        sh "dgoss -g dgoss.yaml validate --format documentation hadolint/hadolint:v1.17.2-20-ge2f77c5-debian"
+        sh "dgoss -g dgoss.yaml run --format documentation hadolint/hadolint:v1.17.2-20-ge2f77c5-debian"
       }
     }
   }
