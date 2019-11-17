5b10229c1bd27cda0670a764ed0fe0f62d382f01
Louie Corbo <lcorbo@cloudbees.com>
2019-11-17 12:27:47 -0600

    Parameterized container destination

--- vars/kanikoBuild.groovy
+++ vars/kanikoBuild.groovy
@@ -1,11 +1,11 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(destination) {
   podTemplate(yaml: "${libraryResource 'pods/kanikoBuild.yaml'}" ) {
     node(POD_LABEL) {
       checkout scm
       container('kaniko') {
-        sh '/kaniko/executor -f `pwd`/Dockerfile -c `pwd` --skip-tls-verify --cache=true --destination=lcorbocb/my-third-repo'
+        sh "/kaniko/executor -f `pwd`/Dockerfile -c `pwd` --skip-tls-verify --cache=true --destination=${destination}"
       }
     }
   }
